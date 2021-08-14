b7be71dfa8825fe8f5262dd44b24b6c557635d48
Brus <201615602e@utea.edu.pe>
2021-08-13 20:32:40 -0500

    Manejo de imágenes y validación

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:productos_app/providers/product_form_provider.dart';
+import 'package:productos_app/services/products_service.dart';
+import 'package:productos_app/services/services.dart';
 import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
 import 'package:productos_app/widgets/product_image.dart';
+import 'package:provider/provider.dart';
 
 class ProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productService = Provider.of<ProductsService>(context);
+
+    return ChangeNotifierProvider(
+      create: (_) => ProductFormProvider(productService.selectedProduct),
+      child: _ProductScreenBody(productService: productService),
+    );
+  }
+}
+
+class _ProductScreenBody extends StatelessWidget {
+  const _ProductScreenBody({
+    Key? key,
+    required this.productService,
+  }) : super(key: key);
+
+  final ProductsService productService;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
+        //keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
         child: Column(
           children: [
             Stack(
               children: [
-                ProductImage(),
+                ProductImage(url: productService.selectedProduct.picture),
                 Positioned(
                   top: 60,
                   left: 20,
@@ -50,6 +75,9 @@ class ProductScreen extends StatelessWidget {
 class _ProductForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productForm = Provider.of<ProductFormProvider>(context);
+    final product = productForm.product;
+
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 10),
       child: Container(
@@ -62,21 +90,39 @@ class _ProductForm extends StatelessWidget {
             children: [
               SizedBox(height: 10),
               TextFormField(
+                initialValue: product.name,
+                onChanged: (value) => product.name = value,
+                validator: (value) {
+                  if (value == null || value.length < 1)
+                    return 'El nombre es obligatorio';
+                },
                 decoration: InputDecorations.authInputDecoration(
                     hintText: 'Nombre del producto', labelText: 'Nombre:'),
               ),
               SizedBox(height: 30),
               TextFormField(
+                initialValue: '${product.price}',
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(
+                      RegExp(r'^(\d+)?\.?\d{0,2}'))
+                ],
+                onChanged: (value) {
+                  if (double.tryParse(value) == null) {
+                    product.price = 0;
+                  } else {
+                    product.price = double.parse(value);
+                  }
+                },
                 keyboardType: TextInputType.number,
                 decoration: InputDecorations.authInputDecoration(
                     hintText: '\$150', labelText: 'Precios:'),
               ),
               SizedBox(height: 30),
               SwitchListTile.adaptive(
-                  value: true,
+                  value: product.available,
                   title: Text('Disponible'),
                   activeColor: Colors.indigo,
-                  onChanged: (value) {}),
+                  onChanged: productForm.updateAvailability),
               SizedBox(height: 30),
             ],
           ),
--- lib/services/products_service.dart
+++ lib/services/products_service.dart
@@ -7,6 +7,7 @@ import 'package:http/http.dart' as http;
 class ProductsService extends ChangeNotifier {
   final String _baseUrl = 'flutter-varios-3bce2-default-rtdb.firebaseio.com';
   final List<Product> products = [];
+  late Product selectedProduct;
 
   bool isLoading = true;
 
